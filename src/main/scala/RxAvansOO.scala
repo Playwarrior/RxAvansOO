553eedb8fb49262c08c9598dbe899ef7421cd159
3615048 <playwarrior2@gmail.com>
2021-03-31 12:15:09 +0200

    opdracht-2 update

--- src/main/scala/RxAvansOO.scala
+++ src/main/scala/RxAvansOO.scala
@@ -4,10 +4,14 @@ object RxAvansOO {
 
   def link[A](next: Observer[A]) = (data: A) => next(data)
 
-  def map[A, B](transform: A => B, next: Observer[A]): Observer[B] = {
-    (_: A) => next(transform(_))
+  def map[A](transform: A => A, next: Observer[A]): Observer[A] = {
+    _ => transform andThen next
   }
 
+  def filter[A](filter: A => Boolean, next: Observer[A]): Observer[A] = {
+    (data: A) => if(filter(data)) next(data)
+  }
 
+  def fork[A](next: List[Observer[A]]):
 
 }
